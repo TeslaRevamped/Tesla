9587a4c6967c273e2efc1df0e9fbf6ef66f57b30
TeslaRevamped <70533421+TeslaRevamped@users.noreply.github.com>
2020-08-31 17:35:09 +0100

    Update TeslaMain.lua

--- master/TeslaMain.lua
+++ master/TeslaMain.lua
@@ -1,5 +1,5 @@
 if Sirhurt then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TeslaRevamped/Tesla/master/master/Sirhurt.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/linsonder6/Tesla/master/Others.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TeslaRevamped/Tesla/master/master/Others.lua"))()
 end
